9f43cb5f71b645e743364eb042ef6580d7c568df
kaser1997 <kaser1997>
2021-10-13 11:54:31 +0300

    add js

---
+++ main.js
@@ -0,0 +1,5 @@
+<sqript>
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+</sqript>
